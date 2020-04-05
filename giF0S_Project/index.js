3bb76636ffbd55cbc8b688d7228aba581d5434f5
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-04-04 19:57:24 -0500

    Pushing changes about Fetch Aoi

--- giF0S_Project/index.js
+++ giF0S_Project/index.js
@@ -4,40 +4,42 @@ console.log(ThemesSelector);
 let Theme2= ThemesSelector[2];
 console.log(Theme2);*/
 
-//Getting the search value
+//CREATING VARIABLES
+//GETTING FIRST ITEM IN SECTION GIFS
+var FirstGif = document.querySelector("#first");
+var Gif1 = FirstGif.firstElementChild;
+console.log(Gif1)
+//GETTING BUTTON
+let ListenSearch = document.querySelector("#ButtonSearch")
+//VARIABLR TO USE IN THE FETCH
+var UrlToFetch = "";
 
-let StringSearch = document.querySelector("#mySearch");
-let x=StringSearch.addEventListener=  () => {
-	console.log(StringSearch.value);
-	
-	
-	return (StringSearch.value)
-	
-}
-GetGifsBySearch(x);
-let MyGifBySearch= new Array ();
+//GETTING SEARCH VALUE AN CALLING THE FETCH
+ListenSearch.addEventListener("click", function () {
+	let StringSearch = document.querySelector("#mySearch");
+	let Valuesearch = (StringSearch.value);
+	console.log(Valuesearch)
+	UrlToFetch = `https://api.giphy.com/v1/gifs/search?api_key=qf6ZWqRanwv9kIXXWpSxlQJmK2zf1UKA&q=${Valuesearch}&limit=25&offset=0&rating=G&lang=en`
+	GetGifsBySearch(UrlToFetch);
+});
 
-function GetGifsBySearch(Search) {
-	return		fetch(`http://api.giphy.com/v1/gifs/search?q=${Search}&api_key=qf6ZWqRanwv9kIXXWpSxlQJmK2zf1UKA`)
-			.then(response => {
-				return response.json();
-			})
-				.then(data => {
-					data
-					console.log(data);
-					console.log(data.data);
-					console.log(data.data[0]);
-					console.log(data.data[0].embed_url);
-					let Divimg1=document.querySelector("#first");
-					let Img1=Divimg1.firstElementChild;
-					console.log(Img1);
-					Img1.setAttribute("src" , data.data[0].bitly_gif_url);
-
-					
-
-					return data
-				}).catch(error => {
-					console.error("fetch failed", error);
-				})
-				
+//FUNCTION TO GET REQUEST BY FETCH
+function GetGifsBySearch(url) {
+	return fetch(url)
+		.then(response => {
+			return response.json();
+		})
+		.then(data => {
+			data;
+			console.log(data);
+			console.log(data.data);
+			console.log(data.data[0]);
+			console.log(data.data[0].embed_url);
+			Gif1.setAttribute("src", data.data[0].embed_url);
+			Gif1.setAttribute("style", "width: 280px");
+			Gif1.setAttribute("style", "height: 280px");
+			return data
+		}).catch(error => {
+			console.error("fetch failed", error);
+		})
 }
