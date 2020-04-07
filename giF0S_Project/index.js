f48f220f588f76faae01b7343afdb86b67881421
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-04-07 17:29:25 -0500

    Saving changes in js file and html file,and new styles in theme2

--- giF0S_Project/index.js
+++ giF0S_Project/index.js
@@ -6,22 +6,24 @@ console.log(Theme2);*/
 
 //CREATING VARIABLES
 //GETTING FIRST ITEM IN SECTION GIFS
-var FirstGif = document.querySelector("#first");
-var Gif1 = FirstGif.firstElementChild;
-console.log(Gif1)
+// var FirstGif = document.querySelector("#first");
+// var Gif1 = FirstGif.firstElementChild;
+// console.log(Gif1)
 //GETTING BUTTON
-let ListenSearch = document.querySelector("#ButtonSearch")
+let ListenSearch = document.querySelector("#buttonSearch")
 //VARIABLR TO USE IN THE FETCH
 var UrlToFetch = "";
+//TODO: FR- CREATE CONST TO ORGANIZE THE URLS DEFINITION   
 
 //GETTING SEARCH VALUE AN CALLING THE FETCH
-ListenSearch.addEventListener("click", function () {
+ListenSearch.addEventListener("click", myFunction);
+
+function myFunction (){
 	let StringSearch = document.querySelector("#mySearch");
 	let Valuesearch = (StringSearch.value);
-	console.log(Valuesearch)
 	UrlToFetch = `https://api.giphy.com/v1/gifs/search?api_key=qf6ZWqRanwv9kIXXWpSxlQJmK2zf1UKA&q=${Valuesearch}&limit=25&offset=0&rating=G&lang=en`
 	GetGifsBySearch(UrlToFetch);
-});
+}
 
 //FUNCTION TO GET REQUEST BY FETCH
 function GetGifsBySearch(url) {
@@ -34,10 +36,22 @@ function GetGifsBySearch(url) {
 			console.log(data);
 			console.log(data.data);
 			console.log(data.data[0]);
-			console.log(data.data[0].embed_url);
-			Gif1.setAttribute("src", data.data[0].embed_url);
-			Gif1.setAttribute("style", "width: 280px");
-			Gif1.setAttribute("style", "height: 280px");
+			console.log(data.data[0].source);
+			let gifSuggest=document.getElementById("gifSuggest");
+			for (let i=0; i<=3; i ++ ){
+				let gifGiphy=data.data[i];
+				let gifInsertIn=document.getElementById(`gifSuggest${i+1}`);
+				let gifImg=document.createElement("img");
+				gifImg.setAttribute("src", gifGiphy.images.original.url);
+				// gifImg.style.width= '280px';
+				// gifImg.setAttribute("height", 280);
+				gifInsertIn.append(gifImg);
+				// gifImg.className="searchedGif";
+				// gifSuggest.appendChild(gifImg);
+			}
+			// Gif1.setAttribute("src", data.data[0].source);
+			// Gif1.setAttribute("style", "width: 280px");
+			// Gif1.setAttribute("style", "height: 280px");
 			return data
 		}).catch(error => {
 			console.error("fetch failed", error);
