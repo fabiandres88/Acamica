84b70638cad2f71d18a035681049d7efb794c9b5
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-05-14 18:25:27 -0500

    Saving change in the pre_Catch.js file in the my gifo function

--- giF0S_Project/Scripts/Pages/preCatch.js
+++ giF0S_Project/Scripts/Pages/preCatch.js
@@ -12,28 +12,27 @@ document.getElementById("logotipe").addEventListener("click", function () {
 //FUNCTION TO SHOW THE UPLOADE GIFS
 window.onload = (paintingMyGuifos);
 function paintingMyGuifos(){
-for (let i = 0; i < localStorage.length; i++) {
-    let urlfetch = "https://api.giphy.com/v1/gifs/";
-    let apiKey = "?api_key=qf6ZWqRanwv9kIXXWpSxlQJmK2zf1UKA";
+for (let i = 0; i < localStorage.length; i++) {    
     if (localStorage.key(i)[0] == "i") {
-        id = localStorage.getItem(localStorage.key(i));
-        let url = urlfetch + id + apiKey;
-        getById(url);
+       let id = localStorage.getItem(localStorage.key(i));
+        getById(id);
     }
 }
 }
 
-function getById(url) {
+function getById(gifid) {
+    
+    let url = `https://api.giphy.com/v1/gifs/${gifid}?api_key=qf6ZWqRanwv9kIXXWpSxlQJmK2zf1UKA`;
     fetch(url)
         .then(response => {
             return response.json();
         })
         .then(data => {
-            data;            
-            let urlTocreate = (data.data.images.downsized_large.url);
+            data;
+            let gifUrl = data.data.images.downsized_large ? data.data.images.downsized_large.url : data.data.images.orignal.url;                        
             let container = document.createElement("div");
             let gif = document.createElement("img");
-            gif.setAttribute("src", urlTocreate);
+            gif.setAttribute("src", gifUrl);
             document.getElementById("containerGifs").appendChild(container);
             container.appendChild(gif);
             return data;
