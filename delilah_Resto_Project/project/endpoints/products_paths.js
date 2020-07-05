20b2d6232340caf4d6732cb8ece372a3ff179e6d
Fabian Ramirez <ramirez1988fabian@gmail.com>
2020-07-04 19:42:37 -0500

    Saving all the paths of the products CRUD, pending to add the validations

--- delilah_Resto_Project/project/endpoints/products_paths.js
+++ delilah_Resto_Project/project/endpoints/products_paths.js
@@ -6,6 +6,7 @@ const validations = require("../validations/products_validations");
 
 module.exports = function (app) {
 
+    //This path allows to get the list of products
     app.get("/products", (req, res) => {
         const query = "SELECT * FROM products";
         sequelize.query(query,
@@ -17,27 +18,55 @@ module.exports = function (app) {
         })
     });
 
-    app.post("/products", (req, res) => {        
+    //This path allows to the manager to add a new product in the menu
+    app.post("/products", (req, res) => {
         const query = "INSERT INTO products (product_name, price, available) VALUES (?,?,?)";
         const { product_name, price, available } = req.body;
         sequelize.query(query,
-            { replacements : [ product_name, price, available ]}
-        ).then((response)=> {
+            { replacements: [product_name, price, available] }
+        ).then((response) => {
             res.json(response);
         }).catch((error) => {
             console.error(error);
         })
     });
 
-    app.put("/products",(req, res) =>{
-        const {id} = req.query;
-        const { price, available } = req.body;        
-        if(price){
-            const query = "UPDATE TABLE products SET price = ? WHERE id = ?";
+    //This path allows to the manager to update the price or the availability of the products
+    app.put("/products", (req, res) => {
+        const { id } = req.query;
+        const { price, available } = req.body;
+        if (price) {
+            const query = "UPDATE products SET price = ? WHERE id = ?";
             sequelize.query(query,
-                    { replacements: [ price ]}
-                )
+                { replacements: [price, id] }
+            ).then((response => {
+                res.status(200).json("Product has been updated");
+            })).catch((error) => {
+                console.error(error);
+            });
+        };
+        if (available) {
+            const query = "UPDATE products SET available = ? WHERE id = ?"
+            sequelize.query(query,
+                { replacements: [available, id] }
+            ).then((response => {
+                res.status(200).json("Product has been updated");
+            })).catch((error) => {
+                console.error(error);
+            });
         }
     });
 
+    //This path allows to the manager to delete a product by "id"
+    app.delete("/products", (req, res) => {
+        const { id } = req.query;
+        const query = "DELETE FROM products WHERE id=?";
+        sequelize.query(query,
+            { replacements: [id] }
+        ).then((response) => {
+            res.status(200).json("Product was deleted");
+        }).catch((error) => {
+            console.error(error);
+        });
+    });
 };
